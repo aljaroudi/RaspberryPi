ae766222e49e8a919157e8b4b29971e938f52fd8
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2021-10-28 18:38:46 -0500

    AirPlay Server

---
+++ AirPlay.sh
@@ -0,0 +1,35 @@
+
+# Recommended: make sure everything is up to date
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install dependencies
+sudo apt-get install -y autoconf automake avahi-daemon build-essential git libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman
+
+# Install AirPlay server. This installs the development version, which supports AirPlay 2. To install the stable version, remove the --branch tag from the command below.
+git clone https://github.com/mikebrady/shairport-sync.git shairport --single-branch --branch development
+
+cd shairport
+
+# Compile
+autoreconf -i -f
+
+./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata
+
+make
+
+sudo make install 
+
+# Start the server
+sudo service shairport-sync start
+
+# Set as a system service so it starts automatically on reboot
+sudo systemctl enable shairport-sync
+
+# Prevent wifi dropouts which may mess up your audio streaming.
+sudo echo "wireless-power off" >> /etc/network/interfaces
+
+# Resources:
+# https://www.jeffreythompson.org/blog/2020/04/29/build-an-airplay-receiver-using-raspberry-pi/
+# https://appcodelabs.com/7-easy-steps-to-apple-airplay-on-raspberry-pi
+# https://github.com/mikebrady/shairport-sync
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RaspberryPi
+
 My RPi tools setup
+
+## Airplay Server
+
+This lets you stream music from any of your eligible Apple devices to any speaker that can take AUX or HDMI input with AirPlay or AirPlay 2. This allows instant connections without Bluetooth manual pairing and a higher quality audio than what is possible with Bluetooth. This also means support for Apple Loseless Audio Codec ([ALAC](https://support.apple.com/en-us/HT212183))\*.
+
+Steps:
+
+1. On your WiFi-enabled Raspberry Pi, run `sh AirPlay.sh`
+1. Connect the Raspberry Pi to an external speaker via AUX or HDMI.
+1. When selecting the audio output on your Apple device, you should see a new device called Raspberry Pi by default.
